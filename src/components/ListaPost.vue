7517f36bfb5682cc53cdeaacc0223e189f3773ca
Carlos Eduardo <carlos@bradigital.com.br>
2020-03-06 18:03:52 -0300

    criado base para aprendizado

---
+++ src/components/ListaPost.vue
@@ -0,0 +1,26 @@
+<template>
+  <carousel :perPage='1'>
+    <slide
+      v-for="post in info" 
+      :key="post.id"> 
+      <a :href="post.id" :v-text="post.id">
+        <img :src="post.image"/>
+      </a>
+    </slide>
+  </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+    export default {
+        name: "ListaPost",
+        props: {
+            info: Array,
+        },
+        components: {
+          Carousel,
+          Slide
+        }
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1> Arquivo de template</h1>
+    <ListaPost :info="info" />
+  </div>
+</template>
+
+<script>
+
+import ListaPost from '@/components/ListaPost.vue'
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    ListaPost,
+  },
+  data(){
+    return {
+      info: []
+    }
+  },
+  mounted: function () {
+      this.readJson()
+  },
+  methods: {
+       async readJson() {
+           await axios.get('/json/banner.json')
+           .then( response => (this.info = response.data));
+       }
+  }
+}
+</script>
